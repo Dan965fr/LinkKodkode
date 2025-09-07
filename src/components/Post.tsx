ec02083e832025d0298242716d16507961708983
Daniel Fromer <daniel.fromer1@gmail.com>
2025-09-07 15:41:52 +0300

    i create css for the post component and import this in the component

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,22 +1,16 @@
-type Posts = {
-    img_url:string,
-    description:string;
-    likes:number,
-    user_name:string,
-    date:string
-}
+import "./Post.css";
+import  type { PostType } from "../types/Posts";
 
-export default function Post({img_url,description,likes,user_name,date}:Posts) {
-
+export default function Post({ img_url, description, likes, user_name, date }: PostType) {
   return (
     <div className="post">
-        <img src={img_url} alt={user_name} className="img_post" />
-        <div className="post-content">
-            <p className="post-description">{description}</p>
-            <p className="post-likes">{likes}</p>
-            <p className="post-user">{user_name}</p>
-            <p className="post-time">{date}</p> 
-        </div>
+      <img src={img_url} alt="post" className="post-image" />
+      <div className="post-content">
+        <p className="post-description">{description}</p>
+        <p className="post-likes"> {likes} likes</p>
+        <p className="post-user_name">from: {user_name}</p>
+        <p className="post-time">{date}</p>
+      </div>
     </div>
-  )
+  );
 }
---
+++ src/types/Posts.ts
@@ -0,0 +1,8 @@
+ export interface PostType  {
+    id:number,
+    img_url:string,
+    description:string;
+    likes:number,
+    user_name:string,
+    date:string
+ }
