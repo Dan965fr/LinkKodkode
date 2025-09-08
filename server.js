93e3e6b4d4a358fd71d12a38f52e9835525bc060
Daniel Fromer <daniel.fromer1@gmail.com>
2025-09-08 15:23:33 +0300

    add cors for web and static for images

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors';
 import postRoutes from "./routes/postsRoutes.js";
 
 const app = express();
@@ -6,9 +7,10 @@ const PORT = 3000;
 
 // Middleware
 app.use(express.json());
+app.use(cors())
 
 // Static folder for images
-app.use("/images", express.static("public/images"));
+app.use(express.static("public"));
 
 // Routes
 app.use("/api/posts", postRoutes);
