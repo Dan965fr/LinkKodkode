a6ad6cff89f24d8ba9f2808ac9ecbd31e650ed90
Daniel Fromer <daniel.fromer1@gmail.com>
2025-09-08 10:50:15 +0300

    create folder controller and service and running the server in server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+
+const PORT = 3000;
+const app = express();
+
+app.use(express.json());
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
